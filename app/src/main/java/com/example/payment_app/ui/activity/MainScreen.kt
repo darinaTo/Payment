c41d495566c7da260e5ef3a740bb5d3ec2bef787
Darina <to.darina.to@gmail.com>
2024-03-13 22:41:12 +0200

    BottomBarNavigation was refactored

--- app/src/main/java/com/example/payment_app/ui/activity/MainScreen.kt
+++ app/src/main/java/com/example/payment_app/ui/activity/MainScreen.kt
@@ -1,129 +1,116 @@
 package com.example.payment_app.ui.activity
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.rounded.AccountBox
-import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.List
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalAbsoluteTonalElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.payment_app.R
-import com.example.payment_app.util.BottomNavigationItem
+import com.example.payment_app.ui.theme.LightBlue
+import com.example.payment_app.util.BottomNavItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage() {
     val items = listOf(
-        BottomNavigationItem(
-            title = stringResource(R.string.home),
-            icon = Icons.Rounded.Home,
-        ),
-        BottomNavigationItem(
-            title = stringResource(R.string.transaction),
-            icon = Icons.Rounded.List,
-        ),
-        BottomNavigationItem(
-            title = stringResource(R.string.accounts),
-            icon = Icons.Rounded.AccountBox,
-        ),
-        BottomNavigationItem(
-            title = stringResource(R.string.accounts),
-            icon = ImageVector.vectorResource(R.drawable.card),
-        )
+        BottomNavItem.Accounts, BottomNavItem.Card,
+        BottomNavItem.Home, BottomNavItem.Transaction
     )
-
-    var selectedItemIndex by rememberSaveable {
-        mutableIntStateOf(0)
-    }
-
     Scaffold(topBar = {
-        TopAppBar(title = {
-            Text(
-                text = stringResource(R.string.money),
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-        }, actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "add",
-                    modifier = Modifier.size(20.dp)
-                )
-            }
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    painter = painterResource(R.drawable.account_balance),
-                    contentDescription = "bank",
-                    modifier = Modifier.size(20.dp)
-
-                )
-            }
-
-        })
+        TopBarNavigation()
     },
         bottomBar = {
-            NavigationBar {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(selected = selectedItemIndex == index,
-                        onClick = {
-                            selectedItemIndex = index
-                        },
+            BottomBarNavigation(items = items)
 
-                        icon = {
-                            Column(
-                                horizontalAlignment = CenterHorizontally,
-                                verticalArrangement = Arrangement.Center,
-                                modifier = Modifier.padding(horizontal = 8.dp)
-                            ) {
-                                val color = if (index == selectedItemIndex) {
-                                    item.selectedColor
-                                } else Color.Gray
-                                Icon(
-                                    imageVector = item.icon,
-                                    contentDescription = item.title,
-                                    tint = color,
-                                    modifier = Modifier.size(30.dp)
-                                )
-
-                                Text(
-                                    text = item.title,
-                                    color = color,
-                                    fontSize = 12.sp
-                                )
-                            }
-
-                        })
-                }
-            }
         }) { paddingValues ->
         TopPayment(modifier = Modifier.padding(paddingValues))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarNavigation() {
+    TopAppBar(title = {
+        Text(
+            text = stringResource(R.string.money),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+    }, actions = {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "add",
+                modifier = Modifier.size(20.dp)
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(R.drawable.account_balance),
+                contentDescription = "bank",
+                modifier = Modifier.size(20.dp)
+
+            )
+        }
+
+    })
+}
+
+@Composable
+fun BottomBarNavigation(items: List<BottomNavItem>) {
+    var selectedItemIndex by rememberSaveable { mutableIntStateOf(-1) }
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = { selectedItemIndex = index },
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.title,
+                        modifier = Modifier.size(30.dp)
+                    )
+                },
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 12.sp,
+                    )
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = LightBlue,
+                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                        LocalAbsoluteTonalElevation.current
+                    ),
+                    selectedTextColor = LightBlue
+                ),
+                modifier = Modifier.padding(horizontal = 4.dp)
+            )
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/payment_app/util/BottomNavItem.kt
@@ -0,0 +1,18 @@
+package com.example.payment_app.util
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.Payment
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(
+    val title: String,
+    val icon: ImageVector,
+) {
+    object Home : BottomNavItem(title = "Home", icon = Icons.Rounded.Home)
+    object Transaction : BottomNavItem(title = "Transaction", icon = Icons.Rounded.List)
+    object Accounts : BottomNavItem(title = "Accounts", icon = Icons.Rounded.AccountBox)
+    object Card : BottomNavItem(title = "My card", icon = Icons.Rounded.Payment)
+}
